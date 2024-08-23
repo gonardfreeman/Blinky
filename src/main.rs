dcb8eec167ccd6a6c098cdb37f9b1694e1a1b6ab
Dmitry Bondarenko <dmitry.bondarenko@silverlinecrm.com>
2024-08-22 21:35:26 -0500

    done

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use embedded_hal::digital::{InputPin, OutputPin, PinState};
+use hal::pac;
+use nrf52833_hal::{
+    self as hal,
+    gpio::{
+        p0::{P0_21, P0_28},
+        Level, Output, PushPull,
+    },
+    pac::Peripherals,
+};
 use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+    let p: Peripherals = pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+
+    let _col1: P0_28<Output<PushPull>> = port0.p0_28.into_push_pull_output(Level::Low);
+    let mut row1: P0_21<Output<PushPull>> = port0.p0_21.into_push_pull_output(Level::Low);
+
+    let mut btn_a = port0.p0_14.into_pullup_input();
+
     rprintln!("Hello world");
-    const GPIO0_PINCNF21_ROW1_ADDR: *mut u32 = 0x5000_0754 as *mut u32;
-    const GPIO0_PINCNF28_COL1_ADDR: *mut u32 = 0x5000_0770 as *mut u32;
-    const DIR_OUTPUT_POS: u32 = 0;
-    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
-    unsafe {
-        write_volatile(GPIO0_PINCNF21_ROW1_ADDR, PINCNF_DRIVE_LED);
-        write_volatile(GPIO0_PINCNF28_COL1_ADDR, PINCNF_DRIVE_LED);
-    }
-    const GPIO0_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
-    const GPIO0_OUT_ROW1_POS: u32 = 21;
-    let mut is_on = false;
     loop {
-        rprintln!("Echo...");
-        unsafe {
-            write_volatile(GPIO0_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
-        }
-        for _ in 0..400_000 {
+        let btn_state = btn_a.is_low().unwrap();
+        rprintln!("btn_a is: {}", btn_state);
+        let _ = row1.set_state(PinState::from(btn_state));
+        for _ in 0..100_000 {
             nop();
         }
-        is_on = !is_on;
     }
 }
